5f3b21ca44664d862e666d5c3bb6d7a742d04261
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-11 12:21:50 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCoreJava
+{
+    class Student
+    {
+        int Studentid;
+        String Studentname;
+        int contactinfo;
+        public void showStudentinfo(int sid,string sname,int phone)
+        {
+            Studentid = sid;
+            Studentname = sname;
+            contactinfo = phone;
+        }
+        public void displayStudentInfo()
+        {
+            Console.WriteLine(Studentid);
+            Console.WriteLine(Studentname);
+            Console.WriteLine(contactinfo);
+        }
+    }
+}
